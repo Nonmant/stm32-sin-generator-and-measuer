ba04b8c2e18a946637191c7daf99e28ba100b0ee
Nonmant <33071228+Nonmant@users.noreply.github.com>
2020-01-19 11:37:42 +0300

    Add files via upload

---
+++ MDK-ARM/RTE/_2_PWM_comPort_timers_4/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: '2 PWM comPort timers 4' 
+ * Target:  '2 PWM comPort timers 4' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+#endif /* RTE_COMPONENTS_H */
